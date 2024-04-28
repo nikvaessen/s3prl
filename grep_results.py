ff0de6cf334deca870df80d7138681ee7c89b07b
Nik Vaessen <git@mail.vaessen.tech>
2024-04-28 15:15:42 +0200

    print results to file

--- grep_results.py
+++ grep_results.py
@@ -120,8 +120,8 @@ def main(result_directory: pathlib.Path):
     print(f"{qbe_mtwv=:.2f}")
 
     # sid
-    si_acc = get_metric(result_directory, "sid", "test acc")
-    print(f"{si_acc=:.2f}")
+    sid_acc = get_metric(result_directory, "sid", "test acc")
+    print(f"{sid_acc=:.2f}")
 
     # asv
     asv_eer = get_metric(result_directory, "asv", "achieves EER")
@@ -144,6 +144,71 @@ def main(result_directory: pathlib.Path):
     sf_cer = get_metric(result_directory, "sf", "slot_value_cer")
     print(f"{sf_f0=:.2f}")
     print(f"{sf_cer=:.2f}")
+
+    # st
+    st_bleu = -1
+
+    # vc
+    vc_mcd = -1
+    vc_wer = -1
+    vc_asv_ar = -1
+
+    # se
+    se_pesq = -1
+    se_stoi = -1
+
+    # ss
+    ss_si_sdri = -1
+
+    # write txt
+    with open(result_directory / "results.csv", "w") as f:
+        # header
+        print("pr", file=f, end=",")
+        print("asr", file=f, end=",")
+        print("asr-ood-es", file=f, end=",")
+        print("asr-ood-ar", file=f, end=",")
+        print("asr-ood-zh-CN", file=f, end=",")
+        print("asr-ood-spoken", file=f, end=",")
+        print("ks", file=f, end=",")
+        print("qbe", file=f, end=",")
+        print("sid", file=f, end=",")
+        print("asv", file=f, end=",")
+        print("sd", file=f, end=",")
+        print("er", file=f, end=",")
+        print("ic", file=f, end=",")
+        print("sf-f0", file=f, end=",")
+        print("sf-cer", file=f, end=",")
+        print("st", file=f, end=",")
+        print("vc-mcd", file=f, end=",")
+        print("vc-wer", file=f, end=",")
+        print("vc-asv-ar", file=f, end=",")
+        print("se-pesq", file=f, end=",")
+        print("se-stoi", file=f, end=",")
+        print("ss", file=f, end="\n")
+
+        # results
+        print(pr_per, file=f, end=",")
+        print(asr_wer, file=f, end=",")
+        print(asr_wer_es, file=f, end=",")
+        print(asr_wer_ar, file=f, end=",")
+        print(asr_cer_zh, file=f, end=",")
+        print(asr_wer_spoken, file=f, end=",")
+        print(ks_acc, file=f, end=",")
+        print(qbe_mtwv, file=f, end=",")
+        print(sid_acc, file=f, end=",")
+        print(asv_eer, file=f, end=",")
+        print(sd_der, file=f, end=",")
+        print(er_acc, file=f, end=",")
+        print(ic_acc, file=f, end=",")
+        print(sf_f0, file=f, end=",")
+        print(sf_cer, file=f, end=",")
+        print(st_bleu, file=f, end=",")
+        print(vc_mcd, file=f, end=",")
+        print(vc_wer, file=f, end=",")
+        print(vc_asv_ar, file=f, end=",")
+        print(se_pesq, file=f, end=",")
+        print(se_stoi, file=f, end=",")
+        print(ss_si_sdri, file=f, end="\n")
 
 
 if __name__ == "__main__":
