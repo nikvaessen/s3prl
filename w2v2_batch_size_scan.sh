f610a512b3d9e918ebe87b0013e3858c3eb5d000
Nik Vaessen <git@mail.vaessen.tech>
2024-05-12 14:57:55 +0200

    just do last one

--- w2v2_batch_size_scan.sh
+++ w2v2_batch_size_scan.sh
@@ -14,7 +14,7 @@ JOBNAME=$(basename "$DIR")
 #find "$DIR" -name "*000000250000*" -exec sbatch --array=0,4,7,10-15,18-21 slurm_snellius.sbatch "$JOBNAME"-250k-"$LR" wav2vec2_custom {} "$LR" \;
 #find "$DIR" -name "*000000350000*" -exec sbatch --array=0,4,7,10-15,18-21 slurm_snellius.sbatch "$JOBNAME"-350k-"$LR" wav2vec2_custom {} "$LR" \;
 
-find "$DIR" -name "*000000100000*" -exec sbatch --array=0,4,7,10-15,18-21 slurm_snellius.sbatch "$JOBNAME"-100k-"$LR" wav2vec2_custom {} "$LR" \;
-find "$DIR" -name "*000000200000*" -exec sbatch --array=0,4,7,10-15,18-21 slurm_snellius.sbatch "$JOBNAME"-200k-"$LR" wav2vec2_custom {} "$LR" \;
-find "$DIR" -name "*000000300000*" -exec sbatch --array=0,4,7,10-15,18-21 slurm_snellius.sbatch "$JOBNAME"-300k-"$LR" wav2vec2_custom {} "$LR" \;
+#find "$DIR" -name "*000000100000*" -exec sbatch --array=0,4,7,10-15,18-21 slurm_snellius.sbatch "$JOBNAME"-100k-"$LR" wav2vec2_custom {} "$LR" \;
+#find "$DIR" -name "*000000200000*" -exec sbatch --array=0,4,7,10-15,18-21 slurm_snellius.sbatch "$JOBNAME"-200k-"$LR" wav2vec2_custom {} "$LR" \;
+#find "$DIR" -name "*000000300000*" -exec sbatch --array=0,4,7,10-15,18-21 slurm_snellius.sbatch "$JOBNAME"-300k-"$LR" wav2vec2_custom {} "$LR" \;
 find "$DIR" -name "*000000400000*" -exec sbatch --array=0,4,7,10-15,18-21 slurm_snellius.sbatch "$JOBNAME"-400k-"$LR" wav2vec2_custom {} "$LR" \;
