853092f3416568f9ab983a15659398afef9be351
Nik Vaessen <git@mail.vaessen.tech>
2024-10-26 09:11:02 +0200

    prep for running vc2 10 min

--- tmlr/run_vc2.sh
+++ tmlr/run_vc2.sh
@@ -9,9 +9,15 @@ ARRAY='1,8,15'
 #sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-5min-300k nanow2v2 ~/data/nanow2v2/extra/vc2/5min/step_0300000.progress.ckpt 1e-4
 #sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-5min-400k nanow2v2 ~/data/nanow2v2/extra/vc2/5min/step_0400000.progress.ckpt 1e-4
 
+# 10 min
+sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-10min-100k nanow2v2 ~/data/nanow2v2/extra/vc2/10min/step_0100000.progress.ckpt 1e-4
+sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-10min-200k nanow2v2 ~/data/nanow2v2/extra/vc2/10min/step_0200000.progress.ckpt 1e-4
+sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-10min-300k nanow2v2 ~/data/nanow2v2/extra/vc2/10min/step_0300000.progress.ckpt 1e-4
+sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-10min-400k nanow2v2 ~/data/nanow2v2/extra/vc2/10min/step_0400000.progress.ckpt 1e-4
+
 # 40 min
 #sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-40min-050k nanow2v2 ~/data/nanow2v2/extra/vc2/40min/step_0050000.progress.ckpt 1e-4
 #sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-40min-100k nanow2v2 ~/data/nanow2v2/extra/vc2/40min/step_0100000.progress.ckpt 1e-4
-sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-40min-200k nanow2v2 ~/data/nanow2v2/extra/vc2/40min/step_0200000.progress.ckpt 1e-4
-sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-40min-300k nanow2v2 ~/data/nanow2v2/extra/vc2/40min/step_0300000.progress.ckpt 1e-4
-sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-40min-400k nanow2v2 ~/data/nanow2v2/extra/vc2/40min/step_0400000.progress.ckpt 1e-4
+#sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-40min-200k nanow2v2 ~/data/nanow2v2/extra/vc2/40min/step_0200000.progress.ckpt 1e-4
+#sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-40min-300k nanow2v2 ~/data/nanow2v2/extra/vc2/40min/step_0300000.progress.ckpt 1e-4
+#sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-40min-400k nanow2v2 ~/data/nanow2v2/extra/vc2/40min/step_0400000.progress.ckpt 1e-4
