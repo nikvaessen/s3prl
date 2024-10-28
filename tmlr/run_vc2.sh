75e6165b4319a68dd199092a8deffc7ba362c06d
Nik Vaessen <git@mail.vaessen.tech>
2024-10-28 23:06:47 +0100

    run vc2 all

--- tmlr/run_vc2.sh
+++ tmlr/run_vc2.sh
@@ -5,10 +5,10 @@
 ARRAY='0,4,10-14'
 
 # 5 min
-#sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-5min-100k nanow2v2 ~/data/nanow2v2/extra/vc2/5min/step_0100000.progress.ckpt 1e-4
-#sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-5min-200k nanow2v2 ~/data/nanow2v2/extra/vc2/5min/step_0200000.progress.ckpt 1e-4
-#sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-5min-300k nanow2v2 ~/data/nanow2v2/extra/vc2/5min/step_0300000.progress.ckpt 1e-4
-#sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-5min-400k nanow2v2 ~/data/nanow2v2/extra/vc2/5min/step_0400000.progress.ckpt 1e-4
+sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-5min-100k nanow2v2 ~/data/nanow2v2/extra/vc2/5min/step_0100000.progress.ckpt 1e-4
+sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-5min-200k nanow2v2 ~/data/nanow2v2/extra/vc2/5min/step_0200000.progress.ckpt 1e-4
+sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-5min-300k nanow2v2 ~/data/nanow2v2/extra/vc2/5min/step_0300000.progress.ckpt 1e-4
+sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-5min-400k nanow2v2 ~/data/nanow2v2/extra/vc2/5min/step_0400000.progress.ckpt 1e-4
 
 # 10 min
 sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-10min-100k nanow2v2 ~/data/nanow2v2/extra/vc2/10min/step_0100000.progress.ckpt 1e-4
@@ -17,8 +17,8 @@ sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-10min-300k nanow2v2 ~/data/
 sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-10min-400k nanow2v2 ~/data/nanow2v2/extra/vc2/10min/step_0400000.progress.ckpt 1e-4
 
 # 40 min
-#sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-40min-050k nanow2v2 ~/data/nanow2v2/extra/vc2/40min/step_0050000.progress.ckpt 1e-4
-#sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-40min-100k nanow2v2 ~/data/nanow2v2/extra/vc2/40min/step_0100000.progress.ckpt 1e-4
-#sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-40min-200k nanow2v2 ~/data/nanow2v2/extra/vc2/40min/step_0200000.progress.ckpt 1e-4
-#sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-40min-300k nanow2v2 ~/data/nanow2v2/extra/vc2/40min/step_0300000.progress.ckpt 1e-4
-#sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-40min-400k nanow2v2 ~/data/nanow2v2/extra/vc2/40min/step_0400000.progress.ckpt 1e-4
+sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-40min-050k nanow2v2 ~/data/nanow2v2/extra/vc2/40min/step_0050000.progress.ckpt 1e-4
+sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-40min-100k nanow2v2 ~/data/nanow2v2/extra/vc2/40min/step_0100000.progress.ckpt 1e-4
+sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-40min-200k nanow2v2 ~/data/nanow2v2/extra/vc2/40min/step_0200000.progress.ckpt 1e-4
+sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-40min-300k nanow2v2 ~/data/nanow2v2/extra/vc2/40min/step_0300000.progress.ckpt 1e-4
+sbatch --array=$ARRAY slurm_snellius.sbatch tmlr-vc2-40min-400k nanow2v2 ~/data/nanow2v2/extra/vc2/40min/step_0400000.progress.ckpt 1e-4
